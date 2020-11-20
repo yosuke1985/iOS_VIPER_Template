9e2f3012bee5d1b32c9cad824d0724d7f8e300fa
yosuke.nakayama <ihatenonoildressing@gmail.com>
2020-11-20 10:52:24 +0900

    update

--- Todo/Presentation/Common/CheckBoxButton.swift
+++ Todo/Presentation/Common/CheckBoxButton.swift
@@ -22,27 +22,11 @@ class CheckBoxButton: UIButton {
         }
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override func awakeFromNib() {
         addTarget(self, action: #selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
         isChecked = false
-        customDesign()
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        customDesign()
-    }
-    
-    override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
-        customDesign()
-    }
-      
-    private func customDesign() {
-        setTitleColor(UIColor(displayP3Red: 79 / 255, green: 172 / 255, blue: 254 / 255, alpha: 1.0), for: .normal)
-    }
-    
+
     @objc func buttonClicked(sender: UIButton) {
         if sender == self {
             isChecked = !isChecked
