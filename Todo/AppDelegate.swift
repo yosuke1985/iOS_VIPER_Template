9b68dd584ce704c06dfcc2ab73c0743742a16578
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2020-12-05 10:03:56 +0900

    added pro, dev

--- Todo/AppDelegate.swift
+++ Todo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        FirebaseApp.configure()
+        configureFirebase()
         
         return true
     }
@@ -32,5 +32,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+extension AppDelegate {
+    func configureFirebase() {
+        #if DEBUG
+            let filePath = Bundle.main.path(forResource: "GoogleService-develop-Info", ofType: "plist")
+        #else
+            let filePath = Bundle.main.path(forResource: "GoogleService-pro-Info", ofType: "plist")
+        #endif
+        let options = FirebaseOptions(contentsOfFile: filePath!)
+        FirebaseApp.configure(options: options!)
     }
 }
